c6738c33dfd9f6e4f1ba876f253037a861ca5bc4
Jin Lee <devkjlee@gmail.com>
2019-09-27 11:57:25 +1000

    Closures and Recursion

--- QuizEngine/Flow.swift
+++ QuizEngine/Flow.swift
@@ -9,12 +9,13 @@
 import Foundation
 
 protocol Router { //an abastraction to the next question handler
-    func routeTo(question: String, answerCallback: @escaping (String) -> Void)
+    typealias AnswerCallback = (String) -> Void
+    func routeTo(question: String, answerCallback: @escaping AnswerCallback)
 }
 
 class Flow {
-    let router: Router
-    var questions: [String]
+    private let router: Router
+    private var questions: [String]
 
     init(questions: [String], router: Router) {
         self.router = router
@@ -23,11 +24,19 @@ class Flow {
     
     func start() {
         if let question = questions.first {
-            router.routeTo(question: question) { [weak self] _ in
-                guard let strongSelf = self else { return }
-                let firstQuestionIndex = strongSelf.questions.firstIndex(of: question)!
-                let nextQuestion = strongSelf.questions[firstQuestionIndex+1]
-                strongSelf.router.routeTo(question: nextQuestion) { _ in }
+            router.routeTo(question: question, answerCallback: routeNext(from: question))
+        }
+    }
+    
+    private func routeNext(from question: String) -> Router.AnswerCallback {
+        return { [weak self] _ in
+            guard let strongSelf = self else { return }
+            
+            if let currentQuestionIndex = strongSelf.questions.firstIndex(of: question) {
+                if currentQuestionIndex + 1 < strongSelf.questions.count {
+                    let nextQuestion = strongSelf.questions[currentQuestionIndex+1]
+                    strongSelf.router.routeTo(question: nextQuestion, answerCallback: strongSelf.routeNext(from: nextQuestion))
+                }
             }
         }
     }
