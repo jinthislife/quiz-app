ea95594ea3c2e54a615b556d1eddee4a59a857ee
Jin Lee <devkjlee@gmail.com>
2019-09-29 22:33:17 +1000

    updated travis configure file to fix test fail

---
+++ QuizApp/QuizAppTests/QuestionViewControllerTest.swift
@@ -0,0 +1,10 @@
+//
+//  QuestionViewControllerTest.swift
+//  QuizAppTests
+//
+//  Created by Jin Lee on 29/9/19.
+//  Copyright © 2019 Jin Lee. All rights reserved.
+//
+
+import Foundation
+import XCTest
